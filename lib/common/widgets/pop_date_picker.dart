2c4929068303afd69928c136dee0c30ca0d5ed09
wanghao <wanghao@oureman.com>
2023-09-13 17:40:16 +0800

    feat：优化自定义日期选择组件逻辑，数据控制更改时间时刷新组件视图

--- lib/common/widgets/pop_date_picker.dart
+++ lib/common/widgets/pop_date_picker.dart
@@ -2,7 +2,7 @@
  * @Author: wanghao wanghao@oureman.com
  * @Date: 2023-07-27 14:48:59
  * @LastEditors: wanghao wanghao@oureman.com
- * @LastEditTime: 2023-08-15 14:28:35
+ * @LastEditTime: 2023-09-13 17:38:40
  * @FilePath: /eatm_manager/lib/common/widgets/pop_date_picker.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -38,6 +38,19 @@ class _PopDatePickerState extends State<PopDatePicker> {
     _controller = TextEditingController(text: widget.value ?? nowFormatDate);
     // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant PopDatePicker oldWidget) {
+    print(widget.value);
+    if (widget.value != _controller.text) {
+      setState(() {
+        _controller =
+            TextEditingController(text: widget.value ?? nowFormatDate);
+      });
+    }
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
